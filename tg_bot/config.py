081cd6395b1a2d7f47353156fb8696c5aa8af8e0
Anonymous5945 <56733554+Anonymous5945@users.noreply.github.com>
2020-08-21 04:05:32 +0530

    Update config.py

--- tg_bot/config.py
+++ tg_bot/config.py
@@ -7,7 +7,7 @@ class Development(Config):
     API_KEY = "1166885639:AAFDYnzxSi9D4m9qefNncAM0NPN6WuYiuKI"  # my api key, as provided by the botfather
     SQLALCHEMY_DATABASE_URI = 'postgresql://username:password@localhost:5432/database'  # sample db credentials
     MESSAGE_DUMP = '' # some group chat that your bot is a member of
-    USE_MESSAGE_DUMP = True
+    USE_MESSAGE_DUMP = False
     SUDO_USERS = [395357593]  # List of id's for users which have sudo access to the bot.
     LOAD = []
     NO_LOAD = ['translation']
